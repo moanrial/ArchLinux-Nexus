3968cf8eb0e4545230e5637440b6096dad239f4e
moanrial <moanrial@gmail.com>
2025-06-24 11:07:03 +0100

    update

--- scripts/atualizar_sistema.sh
+++ scripts/atualizar_sistema.sh
@@ -26,8 +26,7 @@ info "[2/2] A limpar cache de pacotes antigos..."
 # Verificar se o comando paccache existe
 if ! command -v paccache >/dev/null 2>&1; then
 info "O comando 'paccache' não foi encontrado. A instalar 'pacman-contrib'..."
-sudo pacman -S --noconfirm pacman-contrib 
-sudo pacman -S --noconfirm reflector
+sudo pacman -S --noconfirm pacman-contrib reflector
 fi
 
 # Executar a limpeza com segurança (manter 2 versões)
